e2dbe7fec1504ca1765b1710612ac856984cb667
Lindsay Bradford <lindsay.w.bradford@gmail.com>
2015-03-19 19:48:34 +1000

    Adding Copyright Licence detail to project.

--- src/CommandLine/CommandLine.go
+++ src/CommandLine/CommandLine.go
@@ -13,6 +13,7 @@ import (
 
 type Arguments struct {
   Version bool
+  Licence bool
   Problem uint
 }
 
@@ -29,6 +30,14 @@ func (args *Arguments) define() {
     false, 
     "Prints the version number of this utility and exits.",
   )
+
+ flag.BoolVar(
+    &args.Licence, 
+    "Licence", 
+    false, 
+    "Prints the copyright licence this software is released under.",
+  )
+
   
   flag.UintVar(
     &args.Problem,
@@ -44,16 +53,20 @@ func (args *Arguments) define() {
 
 func usageMessage() {
   fmt.Printf("Help for %s\n", GetVersionString())
-  fmt.Println("  --Help                Prints this help message.")
-  fmt.Println("  --Version             Prints the version number of this utility.")
+  fmt.Println("  --Help              Prints this help message.")
+  fmt.Println("  --Version           Prints the version number of this utility.")
+  fmt.Println("  --Licence           Prints the copyright licence this utility is release under.")
   fmt.Println("  --Problem <number>  Specifies problem ID to evaluate.")
   os.Exit(0)
 }
 
 func (args *Arguments) process() {
 
-  if args.Problem == 0 {
-    usageMessage()
+  if args.Licence == true {
+    fmt.Println(
+      getLicenceString(),
+    )
+    os.Exit(0)
   }
 
   if args.Version == true {
@@ -61,6 +74,10 @@ func (args *Arguments) process() {
       GetVersionString(),
     )
     os.Exit(0)
+  }
+
+  if args.Problem == 0 {
+    usageMessage()
   }
 
   var mustExitWithError = false
@@ -82,4 +99,23 @@ func (args *Arguments) process() {
 
 func GetVersionString() string {
   return fmt.Sprintf("%s version %s", os.Args[0], Config.VERSION)
+}
+
+func getLicenceString() string {
+  licence := 
+    "\nCopyright (c) 2015, Lindsay Bradford\n" + 
+    "All rights reserved.\n\n" + 
+    "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n" + 
+    "1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n\n" +
+    "2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\n" + 
+    "3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n" +
+    "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED " +
+    "WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A " +
+    "PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY " +
+    "DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, " +
+    "PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER " +
+    "CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR " +
+     "OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+     
+  return licence
 }
